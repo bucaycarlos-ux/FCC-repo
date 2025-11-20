6ce4b936f6aad91c936232547d91bc86d2e8b5a4
Juan Ocaña <juandy.099@gmail.com>
2025-11-19 19:41:30 -0500

    Seeder para elmodulo de comunidad

---
+++ src/seeders/required/20240807000006-comunidad-interaccion.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert({tableName: 'interaccion', schema: 'fcc_historiaclinica' }, [
+      {
+        descripcion_interaccion: 'Entrega de canasta de alimentos',
+        tipo_interaccion: 'Ayuda Social',
+        fecha_inicio_interaccion: new Date(),
+        fecha_fin_interaccion: new Date(),
+        estado_interaccion: 'Completada',
+        observciones_interaccion: 'Se entregó a tiempo y en buen estado.'
+      },
+      {
+        descripcion_interaccion: 'Taller de costura',
+        tipo_interaccion: 'Capacitación',
+        fecha_inicio_interaccion: new Date(),
+        fecha_fin_interaccion: new Date(),
+        estado_interaccion: 'En Progreso',
+        observciones_interaccion: 'Asistieron 15 personas.'
+      },
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete({tableName: 'interracion', schema: 'fcc_historiaclinica' }, null, {});
+  }
+};
